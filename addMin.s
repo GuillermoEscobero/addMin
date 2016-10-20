4b1ec8bbc318c39af2c634ddd7208ae8741e9844
Raul <Raul>
2016-10-20 16:16:59 +0200

    Added initial code
    Added the provided code in the practice

---
+++ addMin.s
@@ -0,0 +1,19 @@
+.data:
+   .align 2
+
+	dimension: .word 3 
+	matrixA:
+			.float 0.0, -2.3, 4.5 
+			.float 0x7FC00000, 0x7F800000, 0xFF800000
+			.float 3.4, 6.7, 9.9
+
+	matrixB:
+			.float 0.0, -2.3, 4.5 
+			.float 0x7FC00000, 0x7F800000, 0xFF800000
+			.float 3.4, 6.7, 9.9
+
+	matrixC: .space 36
+	matrixD: .space 36 
+
+.text:
+	
