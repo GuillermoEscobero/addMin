324be7680c13890214d1338e753c90ece5243d70
Raul <Raul>
2016-10-25 17:55:21 +0200

    Añadido esqueleto para desarrollar mas tarde

--- addMin.s
+++ addMin.s
@@ -13,10 +13,31 @@
 			.float 3.4, 6.7, 9.9
 
 	matrixC: .space 36
-	matrixD: .space 36 
+	matrixD: .space 36
+
+    i: .word 0
+    j: .word 0
+
+    expMaskA:
+    expMaskB:
+
+    mantMaskA:
+    mantMaskB:
 
 .text:
-	minFloat: 
+    .globl main
+    main:
+        #call the function addMin
+        jal addMin
+
+
+    addMin:
+        #if(addMin == 0) no ha habido error de procesamiento, else addMin = -1 y
+        #se para el programa
+        jr $ra
+
+	minFloat:
 		#hay que usar el stack y guardar el pc
+        jr $ra
 	
 	
