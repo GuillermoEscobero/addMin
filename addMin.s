9b015d9dde1cab16f3dbd53c862c4faabf353fcb
Raul <Raul>
2016-10-25 18:35:32 +0200

    Lo que estoy haciendo ahora mismo, ultra comentado con bastante mierda de debug

--- addMin.s
+++ addMin.s
@@ -15,20 +15,59 @@
 	matrixC: .space 36
 	matrixD: .space 36
 
+
+    #creo que estas mierdas no van aqui, al tener que acceder a ellas mazo,
+    # es mas lento y contraproducente (hay que operar, guardar en la direccion,
+    # luego sacralas...  y entre medias usar registers)
+
     i: .word 0
     j: .word 0
 
-    expMaskA:
-    expMaskB:
+    expMaskA: .space 4 #cual es el tamaño de las masks? 4? puede que sean de tipo .float?
+    expMaskB: .space 4
 
-    mantMaskA:
-    mantMaskB:
+    mantMaskA: .space 4
+    mantMaskB: .space 4
 
 .text:
     .globl main
     main:
+        #load 4 (word length) into t0
+        li $t0 4
+        #load the dimension into t1 to operate with it
+        lw $t1 dimension
+        #as the matrices are square, it means it has (dimension * dimension) elements, obtain it and store it in t1
+        mul $t1 $t1 $t1
+        #obtain the size of the array by multiplying the number of elements by the word length, and store it in a0 (argument)
+        mul $a0 $t0 $t1
+        #load the address of the matrixA into a1
+        la $a1 matrixA
+        #load the address of the matrixB into a2
+        la $a2 matrixB
         #call the function addMin
         jal addMin
+        #check if the parameter returned in v0 is 0, if not there has been an error (go to the end of the program), falta sacar algo por pantalla
+        bnez $v0 end
+
+        #aqui falta el doble for para moverse por las matrices 
+
+        li $t0 0x7F800000
+        li $t1 0x007FFFFF
+
+        lw 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     addMin:
@@ -39,5 +78,5 @@
 	minFloat:
 		#hay que usar el stack y guardar el pc
         jr $ra
-	
+	end:
 	
