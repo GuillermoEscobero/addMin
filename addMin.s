d18720c1930c8691f591e019d22757208f404a2f
Raul <Raul>
2016-10-26 11:50:16 +0200

    Añadido loop para ir entre las matrices

--- addMin.s
+++ addMin.s
@@ -1,15 +1,15 @@
-.data:
+.data
    .align 2
 
 	dimension: .word 3 
 	matrixA:
 			.float 0.0, -2.3, 4.5 
-			.float 0x7FC00000, 0x7F800000, 0xFF800000
+			.word 0x7FC00000, 0x7F800000, 0xFF800000 #aqui si pongo .float estalla
 			.float 3.4, 6.7, 9.9
 
 	matrixB:
 			.float 0.0, -2.3, 4.5 
-			.float 0x7FC00000, 0x7F800000, 0xFF800000
+			.word 0x7FC00000, 0x7F800000, 0xFF800000
 			.float 3.4, 6.7, 9.9
 
 	matrixC: .space 36
@@ -21,7 +21,7 @@
     # luego sacralas...  y entre medias usar registers)
 
     i: .word 0
-    j: .word 0
+    k: .word 0
 
     expMaskA: .space 4 #cual es el tamaño de las masks? 4? puede que sean de tipo .float?
     expMaskB: .space 4
@@ -29,7 +29,7 @@
     mantMaskA: .space 4
     mantMaskB: .space 4
 
-.text:
+.text
     .globl main
     main:
         #load 4 (word length) into t0
@@ -39,7 +39,7 @@
         #as the matrices are square, it means it has (dimension * dimension) elements, obtain it and store it in t1
         mul $t1 $t1 $t1
         #obtain the size of the array by multiplying the number of elements by the word length, and store it in a0 (argument)
-        mul $a0 $t0 $t1
+        mul $a0 $t1 $t0 # 9x4 me da 24... 
         #load the address of the matrixA into a1
         la $a1 matrixA
         #load the address of the matrixB into a2
@@ -48,14 +48,34 @@
         jal addMin
         #check if the parameter returned in v0 is 0, if not there has been an error (go to the end of the program), falta sacar algo por pantalla
         bnez $v0 end
+        #load dimension^2 and aux variable (i)
+        lw $t0 i
+        lw $t1 dimension
+        mul $t1 $t1 $t1
+        #load the address of both matrices (is it okay to use s registers?)
+        la $s0 matrixA
+        la $s1 matrixB
 
-        #aqui falta el doble for para moverse por las matrices 
+    while: bge $t0 $t1 continue
+        #load the word in matrixA and in matrixB
+        lw $t2 ($s0)
+        lw $t3 ($s1)
 
-        li $t0 0x7F800000
-        li $t1 0x007FFFFF
 
-        lw 
+        #if tal y tal pascual
+        #if tal y tal noseque 
+        #else tal tal
 
+
+        #increment the counter by one to check the next element
+        addi $t0 $t0 1
+        #add 4 to the addres of the matrices to retrieve the next element of each matrix in the next iteration
+        addi $s0 $s0 4
+        addi $s1 $s1 4
+        #go to address while to do the loop again
+        b while
+
+    continue:
 
 
 
@@ -73,6 +93,7 @@
     addMin:
         #if(addMin == 0) no ha habido error de procesamiento, else addMin = -1 y
         #se para el programa
+        li $v0 0
         jr $ra
 
 	minFloat:
