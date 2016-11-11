e46e20fe70d04ab611e4c0657fa50864b38c8247
GuillermoEscobero <guillermoescobero@gmail.com>
2016-11-11 01:46:51 +0100

    Añadidos comentarios, addv_1_test1, y mejoras
    Menos registers y probado el test1, hace las 100 iteraciones bien, he
    implementado el uso del stack, tarda como 5 minutos en hacer todo. Hay
    que ver si el resultado de addv_1 y  seqv_1 da bien, no se como va eso

--- lab2.s
+++ lab2.s
@@ -26,77 +26,91 @@
               li $a0 -1
               jr $ra
 
-  continue1:  li $t0 0
-              la $t1 R0
-              la $t2 V0
-              la $t3 V1
+  continue1:  lw $t0 ($a0) # Result Vector (R0)
+              lw $t1 ($a1) # V0
+              lw $t2 ($a2) # V1
 
-              li $t7 4
-              li $t8 1
+              li $t6 0 # COUNTER FOR LOOP (i)
+              li $t7 4 # OFFSET TO ITERATE THROUGH THE ARRAY
+              li $t8 1 # STEP TO BE ADDED TO THE COUNTER EACH ITERATION
 
-      for1:  bge $t0 $a3 end1
+       for1:  bge $t6 $a3 end1
 
-              lw $t4 ($t1)
-              lw $t5 ($t2)
-              lw $t6 ($t3)
+              lw $t3 ($t0) # R0[x]
+              lw $t4 ($t1) # V0[x]
+              lw $t5 ($t2) # V1[x]
 
-              add $t4 $t5 $t6
+              add $t3 $t4 $t5 # R0[x] = V0[x]+V1[x]
 
-              sw $t4 ($t1)
+              sw $t3 ($t0)
 
-              add $t0 $t0 $t8
-              add $t1 $t1 $t7 
-              add $t2 $t2 $t7
-              add $t3 $t3 $t7
+              add $t0 $t0 $t7 # R0[x+1]
+              add $t1 $t1 $t7 # V0[x+1]
+              add $t2 $t2 $t7 # V1[x+1]
+
+              add $t6 $t6 $t8 #i++
 
               b for1
 
         end1: jr $ra
 
-
-    seqv_1:   bge $a3 $0 continue2
+      seqv_1: bge $a3 $0 continue2
               li $a0 -1
               jr $ra
 
-  continue2:  li $t0 0
-              la $t1 R0
-              la $t2 V0
-              la $t3 V1
+   continue2: lw $t0 ($a1) # V0
+              lw $t1 ($a2) # V1
 
-              li $t7 4
-              li $t8 1
+              li $t4 0 # COUNTER FOR LOOP (i)
+              li $t5 4 # OFFSET TO ITERATE THROUGH THE ARRAY
+              li $t6 1 # STEP TO BE ADDED TO THE COUNTER EACH ITERATION
 
-        for2:  bge $a3 $0 end2
+        for2: bge $t4 $a3 end2
 
-              #lw $t4 ($t1)
-              lw $t5 ($t2)
-              lw $t6 ($t3)
+              lw $t2 ($t0) # V0[x]
+              lw $t3 ($t1) # V1[x]
 
-              bne $t5 $t6 else2
+              bne $t2 $t3 else2
 
-              li $t9 1
-              sw $t9 ($t4)
+              add $t0 $t0 $t5 # V0[x+1]
+              add $t1 $t1 $t5 # V1[x+1]
 
-              add $t0 $t0 $t8
-              add $t1 $t1 $t7 
-              add $t2 $t2 $t7
-              add $t3 $t3 $t7
+              add $t4 $t4 $t6 #i++
 
               b for2
 
-        else2:
-              sw $0 ($a0)
+        end2: li $a0 1
+              jr $ra
 
-              b for2
+       else2: li $a0 0
+              jr $ra
 
-        end2: jr $ra
 
 
-addv_1_test1:
+addv_1_test1: li $s0 100 # LOOP LIMIT
+              li $s1 0 # COUNTER FOR LOOP (i)
+              li $s2 1 # STEP TO BE ADDED TO THE COUNTER EACH ITERATION
+              li $s3 4 # OFFSET
+
+        for3: bge $s1 $s0 end3
+
+              sub $sp $sp $s3
+              sw $ra ($sp)
+
               la $a0 R0
               la $a1 V0
               la $a2 V1
               li $a3 10
+
+              jal addv_1
+
+              lw $ra ($sp)
+              add $sp $sp $s3
+
+              add $s1 $s1 $s2
+
+              b for3
+        end3: jr $ra
 
 
 
